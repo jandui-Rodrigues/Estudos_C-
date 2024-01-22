87cc43edd2cd2e33930ba4d62d53aef43600164e
jandui <janduineto18@gmail.com>
2024-01-22 14:34:20 -0300

    Criando um linsta linkada para entender o conceito de Generics

--- dia_04_manipulacao_de_tipos/TiposAvançados.cs
+++ dia_04_manipulacao_de_tipos/TiposAvançados.cs
@@ -11,9 +11,36 @@ namespace TypesAdvanceds
 
     public class GenericList<T>
     {
+        public class Node
+        {
+            public T Value;
+            public Node? Next;
+
+            public Node(T t)
+            {
+                Value = t;
+                Next = null;
+            }
+        }
+
+        private Node Head;
+
+        public GenericList()
+        {
+            Head = null;
+        }
         public void Add(T param)
         {
-
+            if(Head == null)
+            {
+                Head = new Node(param);
+                Console.WriteLine($"Head: {Head.Value}");
+            }
+            else{
+                Node lastNode = Head;
+                while(lastNode.Next != null) lastNode = lastNode.Next;
+                lastNode.Next = new Node(param);
+            }
         }
     }
 }
