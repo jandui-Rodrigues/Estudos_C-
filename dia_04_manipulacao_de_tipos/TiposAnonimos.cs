f9a7f68c8c09a72bcccacb74937f0b3c1eba3a6e
jandui <janduineto18@gmail.com>
2024-01-22 14:43:23 -0300

    tipos Animos em C#

---
+++ dia_04_manipulacao_de_tipos/TiposAnonimos.cs
@@ -0,0 +1,11 @@
+namespace AninymousTypes
+{
+    public class Aninymous
+    {
+        public static void AninymousMethod()
+        {
+            var Aninymous = new { Amount = 108, Message = "Hello" };
+            Console.WriteLine($"Amount: {v.Amount}, Message: {v.Message}");
+        }
+    }
+}
