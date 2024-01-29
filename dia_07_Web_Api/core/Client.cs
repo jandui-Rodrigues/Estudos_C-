69c49fc9c6c6a1a79ecc1dcaead77b199345bd15
jandui <janduineto18@gmail.com>
2024-01-29 10:29:48 -0300

    Adicionando, objeto cliente e Requisiçoes de Cliente

---
+++ dia_07_Web_Api/core/Client.cs
@@ -0,0 +1,10 @@
+namespace dia_07_Web_Api.Core;
+
+public class Client
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public decimal AccountBalance { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
